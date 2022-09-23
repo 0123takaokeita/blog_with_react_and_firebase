764ff6818d4aad12832e145c2a8574dc5c7bffed
Takao <k.takao@neogenia.co.jp>
2022-09-23 21:52:59 +0900

    feat: firebase 初期設定

---
+++ src/firebase.ts
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCN3D3nonrhPxOC2v4902AsrVsabjCiv7s",
+  authDomain: "blog-by-react-vite.firebaseapp.com",
+  projectId: "blog-by-react-vite",
+  storageBucket: "blog-by-react-vite.appspot.com",
+  messagingSenderId: "28492658831",
+  appId: "1:28492658831:web:65a5b88c796536faa258fa",
+  measurementId: "G-SKWDRTKLBL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export  { auth, provider, db, analytics };
