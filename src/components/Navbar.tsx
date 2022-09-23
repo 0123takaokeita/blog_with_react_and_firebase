1aed535562a33b3417abc0bbbd72dbb65fbb8da6
Takao <k.takao@neogenia.co.jp>
2022-09-23 23:12:06 +0900

    feat: fontawsome icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
+
+const houseIcon = <FontAwesomeIcon icon={faHouse} />
+const fileIcon = <FontAwesomeIcon icon={faFilePen} />
+const rightIcon = <FontAwesomeIcon icon={faArrowRightToBracket} />
 
 const Navbar = () => {
   return (
       <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/createpost'>CreatePost</Link>
-        <Link to='/login'>Login</Link>
+        <Link to='/'>{ houseIcon }Home</Link>
+        <Link to='/createpost'>{ fileIcon }CreatePost</Link>
+        <Link to='/login'>{ rightIcon }Login</Link>
       </nav>
   )
 };
