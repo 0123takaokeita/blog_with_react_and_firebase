dc40555f3929416585b5189f3162d9725fde5160
Takao <k.takao@neogenia.co.jp>
2022-09-24 11:23:05 +0900

    fix:  analytics 削除

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
@@ -21,9 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 
-export  { auth, provider, db, analytics };
+export  { auth, provider, db};
