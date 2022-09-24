744da12879235fe1372168ae836caad84a66992d
Takao <k.takao@neogenia.co.jp>
2022-09-24 17:40:58 +0900

    feat: 投稿フォームのvalueを管理するStateを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {ReactElement, useState} from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home       from './components/Home';
 import CreatePost from './components/CreatePost';
 import Login      from './components/Login';
--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import './CreatePost.css';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState('')
+
+  const createPost = () => {
+    console.warn('title:' + title)
+    console.warn('text:' + text)
+  }
+
   return (
     <div className="createPostPage">
       <div className="createPostContainer">
         <h1>記事を投稿する</h1>
         <label>タイトル</label>
-        <input type='text' />
+        <input type='text' onChange={(e) => setTitle(e.target.value)} />
         <label>本文</label>
-        <textarea />
-        <button className="postButton">投稿する</button>
+        <textarea onChange={(e) => setText(e.target.value)} />
+        <button className="postButton" onClick={createPost}>投稿する</button>
       </div>
     </div>
   )
