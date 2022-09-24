4c8e420c94bb0bb10991b8b2d7fb53b19d48a228
Takao <k.takao@neogenia.co.jp>
2022-09-24 11:23:19 +0900

    feat: login機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {ReactElement} from 'react'
+import {ReactElement, useState} from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link, } from 'react-router-dom';
 import Home       from './components/Home';
@@ -7,14 +7,17 @@ import Login      from './components/Login';
 import Logout     from './components/Logout';
 import Navbar     from './components/Navbar';
 
-function App() :ReactElement {
+function App(){
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+  console.info(isAuth)
+
   return(
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home /> }> </Route>
         <Route path="/createpost" element={<CreatePost /> }> </Route>
-        <Route path="/login" element={<Login /> }> </Route>
+        <Route path="/login" element={<Login setIsAuth={ setIsAuth } /> }> </Route>
         <Route path="/logout" element={<Logout /> }> </Route>
       </Routes>
     </Router>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,33 @@
-const Login = () => {
-  return <div>Login</div>
+import React from "react";
+import {signInWithPopup} from "firebase/auth";
+import { auth, provider } from "../firebase";
+import { useNavigate } from 'react-router-dom';
+
+type loginProps = {
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Login = ({ setIsAuth }: loginProps ) => {
+  const navigate = useNavigate();
+
+  const loginwWithGoogle = () => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      console.warn(result)
+      localStorage.setItem('isAuth', 'true')
+      setIsAuth(true)
+      navigate('/');
+    }) .catch((error) => {
+      console.error('error', error)
+    });
+};
+
+  return (
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginwWithGoogle} >Google</button>
+    </div>
+  )
 };
 
 export default Login;
