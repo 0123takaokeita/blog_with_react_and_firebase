4ab7c2e9b4c0609dcfa9e550235ba76c128bb4fb
Takao <k.takao@neogenia.co.jp>
2022-09-24 16:08:36 +0900

    feat: 記事の投稿画面作成

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,5 +1,19 @@
+import React from "react";
+import './CreatePost.css';
+
 const CreatePost = () => {
-  return <div>CreatePost</div>
+  return (
+    <div className="createPostPage">
+      <div className="createPostContainer">
+        <h1>記事を投稿する</h1>
+        <label>タイトル</label>
+        <input type='text' />
+        <label>本文</label>
+        <textarea />
+        <button className="postButton">投稿する</button>
+      </div>
+    </div>
+  )
 };
 
 export default CreatePost;
