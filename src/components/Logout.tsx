31716a7840233550afa4f2ee969c4f9f054286c9
Takao <k.takao@neogenia.co.jp>
2022-09-24 11:37:35 +0900

    feat: logout 機能追加

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,5 +1,33 @@
-const logout = () => {
-  return <div>logout</div>
+import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from 'react-router-dom';
+
+type loginProps = {
+  setIsAuth: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Logout = ({ setIsAuth }: loginProps ) => {
+  const navigate = useNavigate();
+
+  const logoutwWithGoogle = () => {
+    signOut(auth)
+    .then(() => {
+      console.warn('success!!')
+      localStorage.clear()
+      setIsAuth(false)
+      navigate("/login")
+    }) .catch((error) => {
+      console.error('error', error)
+    });
 };
 
-export default logout;
+  return (
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logoutwWithGoogle} >ログアウト</button>
+    </div>
+  )
+};
+
+export default Logout;
