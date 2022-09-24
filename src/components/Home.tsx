f225bd6bedd113a799f9ffc07cecb916701f58d5
Takao <k.takao@neogenia.co.jp>
2022-09-24 18:50:15 +0900

    feat: add CSS to Home components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,23 @@
+import './CreatePost.css';
+import './Home.css';
+
 const Home = () => {
-  return <div>Home</div>
+  return(
+    <div className="homePage">
+      <div className="postContents">
+        <div className="postHeader">
+          <h1>タイトル</h1>
+        </div>
+        <div className="postTextContainer">
+          aaaaaaaaaaaafffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffjyoroyoro
+        </div>
+        <div className="nameAndDeleteButton">
+          <h3>@tkm</h3>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Home;
